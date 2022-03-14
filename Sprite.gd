ce6069773f63847ffd0c61ad644fe2e2c2b49198
Cousals <arnaucasalsf10@gmail.com>
2022-03-14 14:28:18 +0100

    joc_canvis_3
    Moviments WSAD i diagonals
    Area 2D

--- Sprite.gd
+++ Sprite.gd
@@ -1,26 +1,29 @@
 extends Sprite
-
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var velocitat = Vector2(200,0)
+export var velocitat = 300
+var velocitat_angular = 90
+var direccio = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(300,300)
-		
-
+	position = Vector2(100,300)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += velocitat * delta
-	$segonsprite.rotation_degrees += 10
-	rotation_degrees += 10
-	if position.x >= 1024 or position.x <= 0:
-		velocitat.x *= -1
-	if position.y >= 1024 or position.y <= 0:
-		velocitat.y *= -1
+	direccio = Vector2.ZERO
+	if Input.is_action_pressed ("mou_dreta"):	
+		direccio += Vector2.RIGHT
+	if Input.is_action_pressed ("mou_esquerra"):	
+		direccio += Vector2.LEFT
+	if Input.is_action_pressed ("mou_amunt"):	
+		direccio += Vector2.UP
+	if Input.is_action_pressed ("mou_avall"):	
+		direccio += Vector2.DOWN
+	position += direccio.normalized() * velocitat * delta
 	
 	
-	
+	 
+
 
