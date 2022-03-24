a06e850c6331160c9480fac45f62d3d0ea3b0b08
Cousals <arnaucasalsf10@gmail.com>
2022-03-24 08:58:58 +0100

    Dia vaga recuperat
    animated sprite i portals

--- escenas/Personatge2.gd
+++ escenas/Personatge2.gd
@@ -4,7 +4,7 @@ var velocitat_base = 200
 var direccio = Vector2.DOWN
 var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
-var velocitat_salt = - 1000
+var velocitat_salt = - 300
 
 func _physics_process(delta): 
 	velocitat.x = 0 
@@ -18,5 +18,23 @@ func _physics_process(delta):
 	
 	velocitat = move_and_slide (velocitat, Vector2.UP) 
 	
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+	animation(velocitat) 
+	
+func animation(velocitat): 
+	if velocitat.x > 0 and velocitat.y > - 2:
+		$AnimatedSprite.play("corre")
+		$AnimatedSprite.flip_h = false
+	elif velocitat.x < 0 and velocitat.y < - 2:
+		$AnimatedSprite.play("corre")
+		$AnimatedSprite.flip_h = true
+	
+	if abs (velocitat.x) < 0.1 and velocitat.y > -1: 
+		$AnimatedSprite.play ("quiet")
+	
+	if velocitat.y < -2: 
+		$AnimatedSprite.play("salta")
 	
 	
+func _on_Portal_body_entered(body):
+	get_tree ().change_scene("res://escena despres portal.tscn")
